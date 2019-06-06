5dc18d496e7572413aca967b0cad5f770928b34f
Joan Clarke <joanclarke100@gmail.com>
2019-06-05 19:35:33 -0500

    refactor code

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,4 @@
 // Check off specific todo by clicking
 $("li").click(function(){
- //if li is gray 
-  if($(this).css("color") === "rgb(128, 128, 128)"){
-    //turn it to black
-    $(this).css({
-      color: "black",
-      textDecoration: "none"
-    });
-  }
-  //else
-  else{
-      //turn it gray
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through"
-    });
-  }
+$(this).toggleClass("completed");
 });
